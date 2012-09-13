227634e6ce2bfaa4db2e830c294a3c53a4de9328
David Rhoderick <broken.stairs@gmail.com>
2012-09-13 17:39:53 -0400

    Starting to parse XML file
    Only parses to an array currently so the next step is to get the data
    into a database

--- php/xml_itunes_library_parser.php
+++ php/xml_itunes_library_parser.php
@@ -1,36 +1,76 @@
 <?php
 	$filename = "iTunes Music Library.xml";
 	$filepath = "../xml/" . $filename;
-	$depth = array();
+	$index = array();
+	$vals = array();
+	$start_tracks = "<key>Tracks</key>";
 	
-	function startElement($parser, $name, $attrs) 
+	$dataarray = array(
+			"track name"	=>	array(),
+			"artist"		=>	array(),
+			"composer"		=>	array(),
+			"album"			=>	array(),
+			"genre"			=>	array(),
+			"year"			=>	array() );
+	
+	$nexttype = "";
+	
+	function contents( $parser, $data )
 	{
-		global $depth;
-		for ($i = 0; $i < $depth[$parser]; $i++) {
-			echo "  ";
+		global $dataarray;
+		global $tempartist;
+		global $nexttype;
+		
+		if( $nexttype !== "" )
+		{
+			array_push( $dataarray[$nexttype], $data );
 		}
-		echo "$name\n";
-		$depth[$parser]++;
-	}
-	
-	function endElement($parser, $name) 
-	{
-		global $depth;
-		$depth[$parser]--;
-	}
-	
-	$xml_parser = xml_parser_create();
-	xml_set_element_handler($xml_parser, "startElement", "endElement");
-	if (!($fp = fopen($file, "r"))) {
-		die("could not open XML input");
-	}
-	
-	while ($data = fread($fp, 4096)) {
-		if (!xml_parse($xml_parser, $data, feof($fp))) {
-			die(sprintf("XML error: %s at line %d",
-						xml_error_string(xml_get_error_code($xml_parser)),
-						xml_get_current_line_number($xml_parser)));
+		
+		switch( $data )
+		{
+			case "Name":
+				$nexttype = "track name";
+				break;
+			case "Artist":
+				$nexttype = "artist";
+				break;
+			case "Composer":
+				$nexttype = "composer";
+				break;
+			case "Album":
+				$nexttype = "album";
+				break;
+			case "Genre":
+				$nexttype = "genre";
+				break;
+			case "Year":
+				$nexttype = "year";
+				break;
+			default:
+				$nexttype = "";
 		}
+		
 	}
-	xml_parser_free($xml_parser);
+	
+	$parser = xml_parser_create();
+
+	xml_set_character_data_handler($parser, "contents");
+	
+	$fp = fopen($filepath, "r"); 
+	
+	$data = file_get_contents( $filepath );
+	
+	if( !( xml_parse( $parser, $data, feof( $fp ) ) ) )
+	{ 
+		die("Error on line " . xml_get_current_line_number( $parser ) ); 
+	}
+	
+//	echo "End of process line number: " . xml_get_current_line_number( $parser ) . "<br />";
+	
+	xml_parser_free( $parser );
+	$data = "";
+	fclose($fp);
+	
+	var_dump( $dataarray );
+	
 ?>
